56d6a7e880cb0327fead091417ef36fb843787b3
Trapland <89914789+Trapland@users.noreply.github.com>
2023-03-19 18:50:30 +0100

    Add files via upload

--- JS/sound.js
+++ JS/sound.js
@@ -1,13 +1,13 @@
 "use strict";
 
-export const attack = new Audio("../sounds/attack.mp3");
-export const music = new Audio("../sounds/music.mp3");
-export const click = new Audio("../sounds/click.mp3");
-export const win = new Audio("../sounds/win.mp3");
-export const death = new Audio("../sounds/death.mp3");
-export const buy = new Audio("../sounds/buy.mp3");
-export const dmg_magic = new Audio("../sounds/dmg_magic.mp3");
-export const boost_dmg_magic = new Audio("../sounds/boost_dmg_magic.mp3");
+export const attack = new Audio("../attack.mp3");
+export const music = new Audio("../attack.mp3");
+export const click = new Audio("../click.mp3");
+export const win = new Audio("../win.mp3");
+export const death = new Audio("../death.mp3");
+export const buy = new Audio("../buy.mp3");
+export const dmg_magic = new Audio("../dmg_magic.mp3");
+export const boost_dmg_magic = new Audio("../boost_dmg_magic.mp3");
 
 music.volume = 0.03;
 //music.autoplay = true;
